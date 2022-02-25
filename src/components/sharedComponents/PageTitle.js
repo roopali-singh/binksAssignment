d9065f56fed11bb2c305ef3f6c0a08812348135d
Roopali Singh <roopali.singh.222@gmail.com>
2022-02-26 01:45:19 +0530

    Adding ios support for css

--- src/components/sharedComponents/PageTitle.js
+++ src/components/sharedComponents/PageTitle.js
@@ -1,9 +1,15 @@
-import React from "react";
 import "../../style/sharedComponetsStyle/PageTitle.css";
 
 function PageTitle({ title }) {
+  // //--------------------- CHECKING YOUR DEVICE --------------------------//
+
+  var isOnIOS =
+    // navigator.userAgent.match(/Safari/) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPhone/i);
+
   return (
-    <header className="pageTitle">
+    <header className={`pageTitle ${isOnIOS ? "iosDevice" : "nonIosDevice"}`}>
       <h1>{title}</h1>
     </header>
   );
